690fb3c15f308374599ea1e24f8025b46d391d14
ayoubxmahmoud <ayoubmahmoud617@gmail.com>
2025-03-07 18:26:35 +0000

    Install and activate DaisyUI

---
+++ resources/js/Components/DangerButton.jsx
@@ -0,0 +1,16 @@
+export default function DangerButton({
+    className = "",
+    disabled,
+    children,
+    ...props
+}) {
+    return (
+        <button
+            {...props}
+            className={`btn btn-error` + className}
+            disabled={disabled}
+        >
+            {children}
+        </button>
+    );
+}
